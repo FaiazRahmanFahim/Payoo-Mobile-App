6972b4b86ac99cc3a4ea9cde5f61e1b32eae81ec
FaiazRahmanFahim <faiazrahman12.std@gmail.com>
2025-08-26 18:44:05 +0600

    cash out, transfer money, get bonus, pay bill section feature done

--- JS/home.js
+++ JS/home.js
@@ -87,7 +87,7 @@ document
       alert("Invalid Bank Account Number!");
       return;
     }
-    if (addAmount <= 0) {
+    if (isNaN(addAmount) || addAmount <= 0) {
       alert("Please Enter a Valid Amount!");
       return;
     }
@@ -125,7 +125,7 @@ document
       alert("Invalid Agent Number!");
       return;
     }
-    if (Amount <= 0) {
+    if (isNaN(Amount) || Amount <= 0) {
       alert("Please Enter a Valid Amount!");
       return;
     }
@@ -139,6 +139,111 @@ document
     document.getElementById("form-cashOut").reset();
   });
 
+// ------ Transfer Money ------ //
+document
+  .getElementById("btn-sendMoney")
+  .addEventListener("click", function (Event) {
+    Event.preventDefault();
+    //console.log("Send Money Successfully");
+
+    const userAccNum = getInputValueByID("input-userAccNum");
+    const transferAmount = convertInputValueByID("input-transferAmount");
+    const pinNum = convertInputValueByID("input-pinNum-UAN");
+    const totalAmountCount = parseInt(
+      document.getElementById("totalAmount").innerText
+    );
+    console.log(userAccNum, transferAmount, pinNum, totalAmountCount);
+
+    //Validation
+    if (userAccNum === "" || transferAmount === "" || pinNum === "") {
+      alert("Please filled the form!");
+      return;
+    }
+    if (userAccNum.length !== 11) {
+      alert("Invalid Agent Number!");
+      return;
+    }
+    if (isNaN(transferAmount) || transferAmount <= 0) {
+      alert("Please Enter a Valid Amount!");
+      return;
+    }
+    if (pinNum !== validPin) {
+      alert("Invalid Pin Number!");
+      return;
+    }
+
+    const updateTotalAmount = totalAmountCount - transferAmount;
+    document.getElementById("totalAmount").innerText = updateTotalAmount;
+    document.getElementById("form-transferMoney").reset();
+  });
+
+// ------ Get Bonus ------ //
+document
+  .getElementById("btn-getBonus")
+  .addEventListener("click", function (Event) {
+    Event.preventDefault();
+    //console.log("Added Bonus Money Successfully");
+
+    const coupon = convertInputValueByID("input-coupon");
+    //Validation
+    if (isNaN(coupon) || coupon <= 0) {
+      alert("Please Enter a Valid Amount!");
+      return;
+    }
+    const totalAmountCount = parseInt(
+      document.getElementById("totalAmount").innerText
+    );
+    //console.log(couponAmount, totalAmountCount);
+
+    const updateTotalAmount = totalAmountCount + coupon;
+    document.getElementById("totalAmount").innerText = updateTotalAmount;
+    document.getElementById("form-getBonus").reset();
+  });
+
+// ------ Pay Bill ------ //
+document
+  .getElementById("btn-payNow")
+  .addEventListener("click", function (Event) {
+    Event.preventDefault();
+    //console.log("Pay Bill Successfully");
+
+    const selectToPay = getInputValueByID("select-toPay");
+    const billerAccNum = getInputValueByID("input-billerAccNum");
+    const AmountToPay = convertInputValueByID("input-AmountToPay");
+    const pinNum = convertInputValueByID("input-pinNum-PB");
+    const totalAmountCount = parseInt(
+      document.getElementById("totalAmount").innerText
+    );
+    //console.log(selectToPay, billerAccNum, AmountToPay, pinNum, totalAmountCount);
+
+    //Validation
+    if (
+      selectToPay === "" ||
+      billerAccNum === "" ||
+      AmountToPay === "" ||
+      pinNum === ""
+    ) {
+      alert("Please filled the form!");
+      return;
+    }
+    if (billerAccNum.length !== 11) {
+      alert("Invalid Biller Account Number!");
+      return;
+    }
+    if (isNaN(AmountToPay) || AmountToPay <= 0) {
+      alert("Please Enter a Valid Amount!");
+      return;
+    }
+    if (pinNum !== validPin) {
+      alert("Invalid Pin Number!");
+      return;
+    }
+
+    const updateTotalAmount = totalAmountCount - AmountToPay;
+    document.getElementById("totalAmount").innerText = updateTotalAmount;
+    document.getElementById("form-payBill").reset();
+  });
+
 // ------ Section Button ------ //
 document
   .getElementById("btn-sectionAddMoney")
@@ -146,17 +251,54 @@ document
     //console.log("Section Add Money Button Clicked");
     document.getElementById("section-addMoney").style.display = "block";
     document.getElementById("section-cashOut").style.display = "none";
+    document.getElementById("section-transferMoney").style.display = "none";
+    document.getElementById("section-getBonus").style.display = "none";
+    document.getElementById("section-payBill").style.display = "none";
   });
 
 document
   .getElementById("btn-sectionCashOut")
   .addEventListener("click", function () {
     //console.log("Section Add Money Button Clicked");
-    document.getElementById("section-addMoney").style.display = "none";
     document.getElementById("section-cashOut").style.display = "block";
+    document.getElementById("section-addMoney").style.display = "none";
+    document.getElementById("section-transferMoney").style.display = "none";
+    document.getElementById("section-getBonus").style.display = "none";
+    document.getElementById("section-payBill").style.display = "none";
   });
 
+document
+  .getElementById("btn-sectionTransferMoney")
+  .addEventListener("click", function () {
+    //console.log("Section Add Money Button Clicked");
+    document.getElementById("section-transferMoney").style.display = "block";
+    document.getElementById("section-cashOut").style.display = "none";
+    document.getElementById("section-addMoney").style.display = "none";
+    document.getElementById("section-getBonus").style.display = "none";
+    document.getElementById("section-payBill").style.display = "none";
+  });
 
+document
+  .getElementById("btn-sectionGetBonus")
+  .addEventListener("click", function () {
+    //console.log("Section Add Money Button Clicked");
+    document.getElementById("section-getBonus").style.display = "block";
+    document.getElementById("section-transferMoney").style.display = "none";
+    document.getElementById("section-cashOut").style.display = "none";
+    document.getElementById("section-addMoney").style.display = "none";
+    document.getElementById("section-payBill").style.display = "none";
+  });
+
+document
+  .getElementById("btn-sectionPayBill")
+  .addEventListener("click", function () {
+    //console.log("Section Add Money Button Clicked");
+    document.getElementById("section-payBill").style.display = "block";
+    document.getElementById("section-getBonus").style.display = "none";
+    document.getElementById("section-transferMoney").style.display = "none";
+    document.getElementById("section-cashOut").style.display = "none";
+    document.getElementById("section-addMoney").style.display = "none";
+  });
 
 // ------ LogOut Feature ------ //
 document
